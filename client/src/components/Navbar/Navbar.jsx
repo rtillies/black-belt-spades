2a7e7782a90d853bf642af18b484702fdbbaeec1
Richard T <74796181+rtillies@users.noreply.github.com>
2024-04-16 12:56:37 -0400

    Navbar Collapse component

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -3,24 +3,14 @@ import { Link } from "react-router-dom";
 import NavbarBrand from "./NavbarBrand";
 import NavbarList from "./NavbarList";
 import "./Navbar.css";
+import NavbarCollapse from "./NavbarCollapse";
 
 export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary sticky-top">
       <div className="container-fluid">
-      <NavbarBrand />
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
+        <NavbarBrand />
+        <NavbarCollapse />
         <NavbarList />
       </div>
     </nav>
