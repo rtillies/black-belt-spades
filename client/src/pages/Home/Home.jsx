1870d2d47a8c9a47c7d06647999d027acbaaa67c
Richard T <74796181+rtillies@users.noreply.github.com>
2024-04-16 14:44:02 -0400

    Splash image

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,12 +1,15 @@
 import Carousel from "../../components/Carousel/Carousel";
 import PageHeader from "../../components/PageHeader/PageHeader";
+import Splash from "../../components/Splash/Splash";
 import "./home.css";
 
 export default function Home() {
   return (
     <>
-      <PageHeader header="Black Belt Spades" />
-      <Carousel />
+      {/* <PageHeader header="Black Belt Spades" /> */}
+      {/* <Carousel /> */}
+      {/* <img src='images/Black-Belt-Spades.svg' width='100%' /> */}
+      <Splash />
       </>
   );
 }
