91aa1ac7d1dc115133ca576e5f59da9af483bf6f
Richard T <74796181+rtillies@users.noreply.github.com>
2024-04-16 12:50:56 -0400

    Collapsible navbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,16 +1,28 @@
-import React from 'react'
+import React from "react";
 import { Link } from "react-router-dom";
-import NavbarBrand from './NavbarBrand'
-import NavbarList from './NavbarList';
-import './Navbar.css'
+import NavbarBrand from "./NavbarBrand";
+import NavbarList from "./NavbarList";
+import "./Navbar.css";
 
 export default function Navbar() {
   return (
-  <nav className="navbar navbar-expand-lg bg-body-tertiary sticky-top">
-    <div className="container-fluid">
+    <nav className="navbar navbar-expand-lg bg-body-tertiary sticky-top">
+      <div className="container-fluid">
       <NavbarBrand />
-      <NavbarList />
-    </div>
-  </nav>
-  )
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <NavbarList />
+      </div>
+    </nav>
+  );
 }
--- client/src/components/Navbar/NavbarList.jsx
+++ client/src/components/Navbar/NavbarList.jsx
@@ -1,75 +1,76 @@
 import React from "react";
-import { Route, Router, Link } from 'react-router-dom'
+import { Route, Router, Link } from "react-router-dom";
 
 export default function NavbarList() {
   return (
     <>
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <Link className="nav-link" to='/'>
-            Home
-          </Link>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">
-            Lessons
-          </a>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to='/variations'>
-            Variations
-          </Link>
-        </li>
-        <li className="nav-item dropdown">
-          <a
-            className="nav-link dropdown-toggle"
-            href="#"
-            role="button"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-          >
-            Organizations
-          </a>
-          <ul className="dropdown-menu">
-            <li>
-              <a
-                className="dropdown-item"
-                href="https://www.theworldseriesofspades.com/"
-                target="_blank"
-              >
-                The World Series of Spades
-              </a>
-            </li>
-            <li>
-              <a
-                className="dropdown-item"
-                href="https://www.thegameofspades.com/"
-                target="_blank"
-              >
-                The Game of Spades
-              </a>
-            </li>
-            <li>
-              <hr className="dropdown-divider" />
-            </li>
-            <li>
-              <a
-                className="dropdown-item disabled"
-                aria-disabled="true"
-                href="#"
-              >
-                More to come!
-              </a>
-            </li>
-          </ul>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">
-            {" "}
-            Log In
-          </a>
-        </li>
-      </ul>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <li className="nav-item">
+            <Link className="nav-link" to="/">
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/lessons">
+              Lessons
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/variations">
+              Variations
+            </Link>
+          </li>
+          <li className="nav-item dropdown">
+            <a
+              className="nav-link dropdown-toggle"
+              href="#"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Organizations
+            </a>
+            <ul className="dropdown-menu">
+              <li>
+                <a
+                  className="dropdown-item"
+                  href="https://www.theworldseriesofspades.com/"
+                  target="_blank"
+                >
+                  The World Series of Spades
+                </a>
+              </li>
+              <li>
+                <a
+                  className="dropdown-item"
+                  href="https://www.thegameofspades.com/"
+                  target="_blank"
+                >
+                  The Game of Spades
+                </a>
+              </li>
+              <li>
+                <hr className="dropdown-divider" />
+              </li>
+              <li>
+                <a
+                  className="dropdown-item disabled"
+                  aria-disabled="true"
+                  href="#"
+                >
+                  More to come!
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/login">
+              Log In
+            </Link>
+          </li>
+        </ul>
+      </div>
     </>
   );
 }
