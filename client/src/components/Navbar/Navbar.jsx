445599609095d30a60b9ecd86672e2c1de164188
Richard T <74796181+rtillies@users.noreply.github.com>
2024-04-16 11:11:11 -0400

    cleanup

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -10,43 +10,7 @@ export default function Navbar() {
     <div className="container-fluid">
       <NavbarBrand />
       <NavbarList />
-      {/* <div className="collapse navbar-collapse" id="navbarSupportedContent"> */}
-        {/* <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-          <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="index.html">Home</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Lessons</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Variations</a>
-          </li>
-          <li className="nav-item dropdown">
-            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Organizations
-            </a>
-            <ul className="dropdown-menu">
-              <li>
-                <a className="dropdown-item" href="https://www.theworldseriesofspades.com/" target="_blank">The World Series
-                  of Spades</a>
-              </li>
-              <li>
-                <a className="dropdown-item" href="https://www.thegameofspades.com/" target="_blank">The Game of Spades</a>
-              </li>
-              <li>
-                <hr className="dropdown-divider" />
-              </li>
-              <li>
-                <a className="dropdown-item disabled" aria-disabled="true" href="#">More to come!</a>
-              </li>
-            </ul>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#"> Log In</a>
-          </li>
-        </ul> */}
-      </div>
-    {/* </div> */}
+    </div>
   </nav>
   )
 }
