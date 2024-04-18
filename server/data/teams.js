6d0717eb7f4b3811843ddefc82f0d31dfc61d6ed
Richard T <74796181+rtillies@users.noreply.github.com>
2024-04-18 10:26:26 -0400

    update Team data

--- server/data/teams.js
+++ server/data/teams.js
@@ -1,83 +1,91 @@
 const teams = [
   {
-    "id": 1,
+    "teamID": 1,
     "name": "VA Ballerz",
+    "division": "State Property",
     "conference": "East",
     "captain": "Professor",
     "partner": "Omar",
     "location": "Roanoke, VA",
-    "wins": 6,
-    "loss": 0
+    // "wins": 6,
+    // "loss": 0
   },
   {
-    "id": 2,
+    "teamID": 2,
     "name": "JQ Suited",
+    "division": "State Property",
     "conference": "East",
     "captain": "Janet",
     "partner": "Quin",
     "location": "Raleigh, NC",
-    "wins": 2,
-    "loss": 4
+    // "wins": 2,
+    // "loss": 4
   },
   {
-    "id": 3,
+    "teamID": 3,
     "name": "Star Studded",
+    "division": "State Property",
     "conference": "East",
     "captain": "Reece",
     "partner": "Dee",
     "location": "Dallas, TX",
-    "wins": 1,
-    "loss": 5
+    // "wins": 1,
+    // "loss": 5
   },
   {
-    "id": 4,
+    "teamID": 4,
     "name": "That Couple",
+    "division": "State Property",
     "conference": "East",
     "captain": "That Dude",
     "partner": "That Chick",
     "location": "St. Louis, MO",
-    "wins": 3,
-    "loss": 3
+    // "wins": 3,
+    // "loss": 3
   },
   {
-    "id": 5,
+    "teamID": 5,
     "name": "Gerow Animals",
+    "division": "Cash Money",
     "conference": "West",
     "captain": "Chad",
     "partner": "Junbug",
     "location": "Harlem, NY",
-    "wins": 0,
-    "loss": 6
+    // "wins": 0,
+    // "loss": 6
   },
   {
-    "id": 6,
+    "teamID": 6,
     "name": "Team Bui",
+    "division": "Cash Money",
     "conference": "West",
     "captain": "Daniel",
     "partner": "Tiffani",
     "location": "Washington, DC",
-    "wins": 5,
-    "loss": 1
+    // "wins": 5,
+    // "loss": 1
   },
   {
-    "id": 7,
+    "teamID": 7,
     "name": "Jerzeys Finest",
+    "division": "Cash Money",
     "conference": "West",
     "captain": "Marcus",
     "partner": "Sniper",
     "location": "Teaneck, NJ",
-    "wins": 3,
-    "loss": 3
+    // "wins": 3,
+    // "loss": 3
   },
   {
-    "id": 8,
+    "teamID": 8,
     "name": "Knuck If You Stuck",
+    "division": "Cash Money",
     "conference": "West",
     "captain": "Aceito",
-    "partner": "Deborah",
+    "partner": "Borah",
     "location": "Hartford, CT",
-    "wins": 4,
-    "loss": 2
+    // "wins": 4,
+    // "loss": 2
   }
 ]
 
