af790d53908f88969292c27155ec58a4a7a7bfee
Richard T <74796181+rtillies@users.noreply.github.com>
2024-04-18 09:24:05 -0400

    create Division schema

---
+++ server/db/Division.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+
+const divisionSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    unique: true, // unique index
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  conference: {
+    // type: mongoose.SchemaTypes.ObjectId,
+    type: mongoose.SchemaTypes.String,
+    ref: "Conference",
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    immutable: true,
+    default: () => Date.now(),
+  },
+  updatedAt: {
+    type: Date,
+    default: () => Date.now(),
+  },
+  // timestamps: true,
+});
+
+divisionSchema.statics.findByName = function(name) {
+  return this.findOne({name: new RegExp(name, 'i')})
+}
+
+divisionSchema.query.byName = function(name) {
+  return this.where({name: new RegExp(name, 'i')})
+}
+
+divisionSchema.pre('save', function(next) {
+  this.updatedAt = Date.now()
+  next()
+}) 
+
+divisionSchema.methods.updateDivision = function() {
+  console.log(`${this.name} Division updated.`);
+}
+
+// divisionSchema.post('save', function(doc, next) {
+//   doc.updateConf()
+//   next()
+// }) 
+
+
+module.exports = mongoose.model("Division", divisionSchema);
