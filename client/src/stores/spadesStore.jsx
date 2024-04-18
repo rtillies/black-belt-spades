282b61bec6520f167c37930a1ed98b2ad0b7e5f0
Richard T <74796181+rtillies@users.noreply.github.com>
2024-04-18 15:17:22 -0400

    try-catch block around data fetch

--- client/src/stores/spadesStore.jsx
+++ client/src/stores/spadesStore.jsx
@@ -2,63 +2,24 @@ import {create} from 'zustand'
 import axios from "axios";
 
 const spadesStore = create((set) => ({
-  conferences: [],
+  conferences: null,
   divisions: null,
   teams: null,
   games: null,
 
   getData: async (table) => {
-    console.log("Hello");
-    const res = await axios.get(`/${table}`)
-    console.log(`${table}`, res.data);
-
-    set({
-      [table]: res.data,
-    })
+    // console.log("Hello");
+    try {
+      const res = await axios.get(`/${table}`)
+      console.log(`${table}`, res.data);
+  
+      set({
+        [table]: res.data,
+      })
+    } catch (error) {
+      console.log(error);
+    }
   }
-
-  // getCards: async() => {
-  //   const {deckID, deck} = cardsStore.getState() || 'new'
-  //   const count = 54 // cards remaining
-
-  //   if (deckID === null) {
-  //     // set({deckID: 'new'})
-  //   }
-
-  //   await axios.get(`${deckID}/shuffle/`)
-  //   const res = await axios.get(`${deckID}/draw/?count=${count}`)
-
-  //   console.log(deckID);
-  //   console.log('data', res.data.cards);
-  //   set({
-  //     deck: res.data.cards,
-  //     showDeck: res.data.cards
-  //   })
-  // },
-
-  // filter: (term) => {
-  //   const {deck, showDeck} = cardsStore.getState()
-
-  //   console.log('filter', term);
-  //   console.log('deck filter', deck);
-
-  //   if(term.toLowerCase() === 'all') {
-  //     set({
-  //       showDeck: deck
-  //     })
-  //     return
-  //   }
-
-  //   const filteredDeck = deck.filter((card) => {
-  //     return (card.value.toLowerCase() === term || card.suit.toLowerCase() === term)
-  //   })
-
-  //   console.log('new deck', filteredDeck);
-
-  //   set({
-  //     showDeck: filteredDeck
-  //   })
-  // },
 
 }))
 
