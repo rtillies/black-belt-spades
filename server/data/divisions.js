9bfbd6de71e1d566a4947873f20b85e394d507ea
Richard T <74796181+rtillies@users.noreply.github.com>
2024-04-18 10:17:47 -0400

    Create division data

---
+++ server/data/divisions.js
@@ -0,0 +1,50 @@
+const divisions = [
+  {
+    "name": "State Property",
+    "description": "State Property division, Eastern conference",
+    "conference" : "East"
+  },
+  {
+    "name": "Bad Boy",
+    "description": "Bad Boy division, Eastern conference",
+    "conference" : "East"
+  },
+  {
+    "name": "So So Def",
+    "description": "So So Def division, Eastern conference",
+    "conference" : "East"
+  },
+  {
+    "name": "Cash Money",
+    "description": "Cash Money division, Western conference",
+    "conference" : "West"
+  },
+  {
+    "name": "Death Row",
+    "description": "Death Row division, Western conference",
+    "conference" : "West"
+  },
+  {
+    "name": "G.O.O.D.",
+    "description": "G.O.O.D. division, Western conference",
+    "conference" : "West"
+  },
+]
+
+module.exports = divisions;
+
+// Cash Money
+// Death Row
+// G.O.O.D.
+
+// State Property
+// Bad Boy
+// So So Def
+
+// Aftermath
+// Ruff Ryders
+// State Property
+
+// Death Row
+// No Limit
+// Roc-A-Fella
