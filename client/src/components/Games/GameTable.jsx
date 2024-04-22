a50c703a318f0da31d15697f627c88be9b79f671
Richard T <74796181+rtillies@users.noreply.github.com>
2024-04-22 14:21:54 -0400

    format

--- client/src/components/Games/GameTable.jsx
+++ client/src/components/Games/GameTable.jsx
@@ -7,24 +7,24 @@ export default function GameTable() {
 
   return (
     <>
-        <div className="table-display">
-      <table className="table table-striped table-hover table-bordered">
-        <thead>
-          <tr>
-            <th scope="col">Game ID</th>
-            <th scope="col">Date</th>
-            <th scope="col">Home Team</th>
-            <th scope="col">Away Team</th>
-            <th scope="col">Final Score</th>
-          </tr>
-        </thead>
-        <tbody>
-          {store.games &&
-            store.games.map((game, index) => {
-              return <Game key={index} game={game} />;
-            })}
-        </tbody>
-      </table>
+      <div className="table-display">
+        <table className="table table-striped table-hover table-bordered">
+          <thead>
+            <tr>
+              <th scope="col">Game ID</th>
+              <th scope="col">Date</th>
+              <th scope="col">Home Team</th>
+              <th scope="col">Away Team</th>
+              <th scope="col">Final Score</th>
+            </tr>
+          </thead>
+          <tbody>
+            {store.games &&
+              store.games.map((game, index) => {
+                return <Game key={index} game={game} />;
+              })}
+          </tbody>
+        </table>
       </div>
     </>
   );
