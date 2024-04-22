dbe58a8167edbda085c88dc904c2b15e60a5fa3e
Richard T <74796181+rtillies@users.noreply.github.com>
2024-04-22 13:51:20 -0400

    fix navbar brand link

--- client/src/components/Navbar/NavbarBrand.jsx
+++ client/src/components/Navbar/NavbarBrand.jsx
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom";
 export default function NavbarBrand() {
   return (
     <>
+    <Link className="navbar-brand" to="/">
       <img
         id="black-belt"
         src="images/black-belt.png"
         alt="Logo"
         className="d-inline-block align-text-top"
       />
-      <Link className="navbar-brand" href="/">
         Black Belt Spades
       </Link>
     </>
