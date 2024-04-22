08b87d5e83a665d0fc54f7c4b37e2cd0bd3beef5
Richard T <74796181+rtillies@users.noreply.github.com>
2024-04-22 14:45:28 -0400

    hide buttons

--- client/src/components/PageHeader/PageHeader.jsx
+++ client/src/components/PageHeader/PageHeader.jsx
@@ -4,8 +4,8 @@ export default function PageHeader({ header }) {
   return (
     <div className="page-header p-4">
       <h1>{header}</h1>
-      <HeaderButton text="All Teams" hidden="false" />
-      <HeaderButton text="All Games" hidden="true" />
+      {/* <HeaderButton text="All Teams" hidden="false" /> */}
+      {/* <HeaderButton text="All Games" hidden="true" /> */}
     </div>
   );
 }
