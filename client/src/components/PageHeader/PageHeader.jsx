be4b04a90929eb081cb44eccce6ffa12e51991af
Richard T <74796181+rtillies@users.noreply.github.com>
2024-04-22 14:20:35 -0400

    Add button

--- client/src/components/PageHeader/PageHeader.jsx
+++ client/src/components/PageHeader/PageHeader.jsx
@@ -2,6 +2,7 @@ export default function PageHeader({ header }) {
   return (
     <div className="page-header p-4">
       <h1>{header}</h1>
+      <button className="btn btn-outline-primary mx-4 page-btn">All Teams</button>
     </div>
   );
 }
