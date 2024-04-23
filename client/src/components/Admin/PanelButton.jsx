7dffd444f12ff59bc1a47e103119514df65e886b
Richard T <74796181+rtillies@users.noreply.github.com>
2024-04-23 09:47:44 -0400

    get accurate button

--- client/src/components/Admin/PanelButton.jsx
+++ client/src/components/Admin/PanelButton.jsx
@@ -10,9 +10,45 @@ export default function PanelButton({color, text, group}) {
 
   const handleButtonClick = (e) => {
     const value = e.target.value.toLowerCase()
-    console.log(value);
+    // console.log(value);
     
     switch(value) {
+      case 'add-game':
+        console.log('Add game');
+        break;
+      case 'update-game':
+        console.log('Update game');
+        break;
+      case 'delete-game':
+        console.log('Delete game');
+        break;
+      case 'add-team':
+        console.log('Add team');
+        break;
+      case 'update-team':
+        console.log('Update team');
+        break;
+      case 'delete-team':
+        console.log('Delete team');
+        break;
+      case 'add-division':
+        console.log('Add division');
+        break;
+      case 'update-division':
+        console.log('Update division');
+        break;
+      case 'delete-division':
+        console.log('Delete division');
+        break;
+      case 'add-conference':
+        console.log('Add conference');
+        break;
+      case 'update-conference':
+        console.log('Update conference');
+        break;
+      case 'delete-conference':
+        console.log('Delete conference');
+        break;
       default:
         return
     }
