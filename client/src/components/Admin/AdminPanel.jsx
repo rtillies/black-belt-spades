55130c1dbb6c8c402bfe143577c7243ee052c065
Richard T <74796181+rtillies@users.noreply.github.com>
2024-04-23 09:30:57 -0400

    update admin dashboard

--- client/src/components/Admin/AdminPanel.jsx
+++ client/src/components/Admin/AdminPanel.jsx
@@ -5,14 +5,14 @@ export default function AdminPanel({header}) {
   const single = header.slice(0,header.length - 1)
   // console.log(single);
   const actions = ['Add', 'Update', 'Delete']
-  const colors = ['primary', 'success', 'danger', 'info', 'secondary', 'dark', ]
+  // const colors = ['primary', 'success', 'danger', 'info', 'secondary', 'dark', ]
 
   return (
     <>
       <div className="admin-panel mx-4">
-        <h2>{header}</h2>
+        <h3>{header}</h3>
         {actions.map((action, i) => {
-          return <PanelButton key={i} color={i} text={`${action} ${single}`} />
+          return <PanelButton key={i} color={i} text={`${action}`} group={`${single}`} />
         })}
         <hr />
       </div>
--- client/src/components/Admin/PanelButton.jsx
+++ client/src/components/Admin/PanelButton.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import { Link, useNavigate } from 'react-router-dom';
 import spadesStore from '../../stores/spadesStore';
 
-export default function PanelButton({color, text}) {
+export default function PanelButton({color, text, group}) {
   const navigate = useNavigate()
   const store = spadesStore();
   const colors = ['primary', 'success', 'danger',] // 'info', 'secondary', 'dark', ]
 
 
   const handleButtonClick = (e) => {
-    const value = e.target.value
+    const value = e.target.value.toLowerCase()
     console.log(value);
     
     switch(value) {
@@ -20,7 +20,7 @@ export default function PanelButton({color, text}) {
 
   return (
     <>
-      <button value={text} 
+      <button value={`${text}-${group}`} 
         onClick={handleButtonClick}
         className={`btn btn-outline-${colors[color]} mx-4 panel-btn`}>{text}</button>
     </>
